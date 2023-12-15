a8182348ddfd83967df5360b4954b0f3c8d8d726
Huzaifa <syedmhuzaifa234@gmail.com>
2023-12-15 14:41:48 +0500

    Friday

---
+++ PFTestingFinal/PFTestingFinal/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace PFTestingFinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.add = new System.Windows.Forms.Button();
+            this.refresh = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(78, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(141, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(78, 75);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(141, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(78, 315);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(112, 23);
+            this.add.TabIndex = 2;
+            this.add.Text = "Add";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // refresh
+            // 
+            this.refresh.Location = new System.Drawing.Point(232, 315);
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(112, 23);
+            this.refresh.TabIndex = 3;
+            this.refresh.Text = "Refresh";
+            this.refresh.UseVisualStyleBackColor = true;
+            this.refresh.Click += new System.EventHandler(this.refresh_Click);
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(382, 315);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(112, 23);
+            this.clear.TabIndex = 4;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(264, 35);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(513, 260);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.refresh);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button refresh;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ PFTestingFinal/PFTestingFinal/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace PFTestingFinal
+{
+    public partial class Form1 : Form
+    {
+
+        static string path = @"C:\Users\Huzyfa\source\repos\PFTestingFinal\PFTestingFinal\bin\Debug\stdDatabase.txt";
+        List<List<String>> data = new List<List<String>>();
+        int counter = 0;
+        int refreshCounter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.Columns.Add("ID", 80);
+            listView1.Columns.Add("Name", 120);
+            
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            refreshCounter = 0;
+            data.Add(new List<String>());
+            data[counter].Add(textBox1.Text);
+            data[counter].Add(textBox2.Text);
+
+            ListViewItem items = new ListViewItem(data[counter][0]);
+            items.SubItems.Add(data[counter][1]);
+            listView1.Items.Add(items);
+            counter++;
+
+            textBox1.Clear();
+            textBox2.Clear();
+
+
+
+            FileStream stream = new FileStream("stdDatabase.txt", FileMode.Append, FileAccess.Write);
+            using (StreamWriter writer = new StreamWriter(stream))
+            {
+
+
+                for (int j = 0; j < counter; j++)
+                {
+                    foreach (var item in data[j])
+                    {
+                        writer.Write(item + ",");
+                    }
+
+
+                    writer.WriteLine("");
+                }
+            }
+            stream.Close();
+        }
+
+        private void refresh_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+
+
+            if (refreshCounter == 0)
+            {
+                List<List<String>> read = new List<List<String>>();
+                int readCounter = 0;
+
+                int TotalRows = File.ReadAllLines(path).Length;
+                //Students = new string[TotalRows, 4];
+                string line = "";
+                StreamReader sr = new StreamReader(path);
+                int Row = 0;
+
+                while ((line = sr.ReadLine()) != null)
+
+                {
+                    read.Add(new List<String>());
+
+                    string[] temp = line.Split(',');
+                    for (int i = 0; i < temp.Length; i++)
+                    {
+                        read[readCounter].Add(temp[i]);
+                        //Students[Row, i] = temp[i];
+                    }
+                    Row++;
+                    readCounter++;
+                }
+                
+                sr.Close();
+
+
+                for (int i = 0; i < readCounter; i++)
+                {
+
+
+                   
+
+                    ListViewItem stud = new ListViewItem(read[i][0]);
+                    stud.SubItems.Add(read[i][1]);
+                    listView1.Items.Add(stud);
+
+
+                }
+
+                refreshCounter++;
+            }
+
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+        }
+    }
+}
